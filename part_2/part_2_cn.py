e372ce5e0cdf8468ae0774a0cfe9f0eb3362b020
HunJer93 <jhunton22@gmail.com>
2024-12-01 10:44:49 -0500

    added CN for part 2 cn

--- part_2/part_2_cn.py
+++ part_2/part_2_cn.py
@@ -50,3 +50,42 @@ test_set = test_data_generator.flow_from_directory(
 # END DATA PRE-PROCESSING
 ##################
 
+
+##################
+# BUILD THE CNN
+##################
+
+cnn = tf.keras.models.Sequential()
+
+# create the convolutional layer
+
+cnn.add(tf.keras.layers.Conv2D(
+    filters=32, # 32 is common filter size
+    kernel_size=3, # 3x3 filter
+    activation='relu', # using rectifier activation function
+    input_shape=[image_size, image_size, 3 ] # dimension size of the images (used same dimensions from above) and 3 (r, g, b) for color images (we would use 1 if the images were black and white) (only needed on first layer)
+))
+
+# create pooling layer
+cnn.add(tf.keras.layers.MaxPool2D(
+    pool_size=2, # when creating our pool size, we are moving 2x2 frames (pixles)
+    strides=2 # moving 2x2 frame (pool_size) 2 pixels at a time
+))
+
+# create 2nd convolutional layer
+
+cnn.add(tf.keras.layers.Conv2D(
+    filters=32, # 32 is common filter size
+    kernel_size=3, # 3x3 filter
+    activation='relu', # using rectifier activation function
+))
+
+# create 2nd pooling layer
+cnn.add(tf.keras.layers.MaxPool2D(
+    pool_size=2, # when creating our pool size, we are moving 2x2 frames (pixles)
+    strides=2 # moving 2x2 frame (pool_size) 2 pixels at a time
+))
+
+
+
+
