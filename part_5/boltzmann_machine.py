dc6ebcbecae25560b26e02d5f5ba93b28a621c25
HunJer93 <jhunton22@gmail.com>
2025-02-02 13:08:36 -0500

    prepared training and test sets from training and test data

--- part_5/boltzmann_machine.py
+++ part_5/boltzmann_machine.py
@@ -16,3 +16,13 @@ movies = pd.read_csv('ml-1m/movies.dat', sep= '::', header= None, engine= 'pytho
 users = pd.read_csv('ml-1m/users.dat', sep= '::', header= None, engine= 'python', encoding= 'latin-1')
 ratings = pd.read_csv('ml-1m/ratings.dat', sep= '::', header= None, engine= 'python', encoding= 'latin-1')
 
+# prepare the training sets and test sets for analysis
+# dataset is delimited by a tab. There are other data sets in the folder for more accurate analysis,
+# but for the sake of example, we're only training this model with one training set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter= '\t')
+training_set = np.array(training_set, dtype= 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter= '\t')
+test_set = np.array(test_set, dtype= 'int')
+
+print(training_set)
+print(test_set)
