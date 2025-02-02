101a4b26e609228ba73173f126ffc7b5ff006bf5
HunJer93 <jhunton22@gmail.com>
2025-02-02 12:59:44 -0500

    added imports for data sets

--- part_5/boltzmann_machine.py
+++ part_5/boltzmann_machine.py
@@ -1,2 +1,18 @@
 # Restricted Boltzmann Machine for recommending if a user will like a movie or not
 # data set for movies come from https://grouplens.org/datasets/movielens/
+
+# import libraries
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+# import the datasets for movies, users, and ratings
+movies = pd.read_csv('ml-1m/movies.dat', sep= '::', header= None, engine= 'python', encoding= 'latin-1')
+users = pd.read_csv('ml-1m/users.dat', sep= '::', header= None, engine= 'python', encoding= 'latin-1')
+ratings = pd.read_csv('ml-1m/ratings.dat', sep= '::', header= None, engine= 'python', encoding= 'latin-1')
+
